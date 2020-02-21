8a58708a555962ae1c9ae1ff30a0cc9ea3ac5761
Pavan Kumar <pavan@testpress.in>
2020-02-21 18:14:05 +0530

    Change package name

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ def get_package_data(package):
     return {package: filepaths}
 
 
-version = get_version('rest_framework')
+version = get_version('rest_framework3')
 
 
 if sys.argv[-1] == 'publish':
